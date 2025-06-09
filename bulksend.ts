2a4e30926ae6703d97223c62b942ee085485b77b
0x_antoni <0x_antoni@163.com>
2025-06-09 18:12:58 +0800

    optimize send tx

--- bulksend.ts
+++ bulksend.ts
@@ -13,50 +13,59 @@ const { processId, totalTxNum } = parseArgv()
 // 发送交易函数
 async function sendTransactions(wallets: ethers.Wallet[]) {
     const perAccountBatchSendNumOnce = totalTxNum / WALLET_COUNT;
-    const gasPrice = await provider.getFeeData().then(feeData => feeData.gasPrice);
-    let toPrivkey = processId * totalTxNum + 1; // 接受者地址的私钥
-    console.log("start toPrivkey:", toPrivkey, "perAccountBatchSendNumOnce", perAccountBatchSendNumOnce)
+    let toPrivkeyStartInTotal = processId * totalTxNum + 1; // 接受者地址的私钥
+    console.log("start toPrivkeyStartInTotal:", toPrivkeyStartInTotal, "perAccountBatchSendNumOnce", perAccountBatchSendNumOnce)
 
-    const lastTxs = []
+    // const lastTxs: string[] = []
+
+    const promises = []
     for (let i = 0; i < wallets.length; i++) {
-        const wallet = wallets[i];
-
-        // 获取当前 nonce
-        var nonce = await wallet.getNonce();
-        for (let j = 0; j < perAccountBatchSendNumOnce; j++) {
-
-            const to = config.IS_RECEIVER_BY_PK ?
-                new ethers.Wallet(numberToPrivateKey(toPrivkey), provider).address :
-                wallet.address
-
-            try {
-                // 创建交易数据
-                const tx: ethers.TransactionRequest = {
-                    nonce,
-                    gasLimit: 21000, // 适用于简单的转账交易
-                    gasPrice: gasPrice!,
-                    to,
-                    value: ethers.parseEther("0.000000000000000001"), // 每次转账 1 wei
-                    data: "0x", // 数据字段为空
-                };
-
-                // 签名并发送交易
-                const txResp = await wallet.sendTransaction(tx);
-                console.log(`sent tx, process ${processId} wallet ${i} nonce ${nonce} to ${to} pk ${toPrivkey} hash ${txResp.hash}`);
-                nonce++
-                toPrivkey++
-
-                if (j == perAccountBatchSendNumOnce - 1) {
-                    lastTxs.push(txResp.hash)
-                }
-            } catch (error) {
-                console.error(`Failed to send transaction for ${wallet.address}:`, error);
-            }
-        }
+        promises.push(sendTxByOneWallet(i, wallets[i], perAccountBatchSendNumOnce, toPrivkeyStartInTotal + (perAccountBatchSendNumOnce * i)));
     }
+    const lastTxs = await Promise.all(promises)
 
     await Promise.all(lastTxs.map(tx => provider.waitForTransaction(tx)))
     console.log("all txs mined")
+}
+
+async function sendTxByOneWallet(walletIndex: number, wallet: ethers.Wallet, perAccountBatchSendNumOnce: number, toPrivkeyStart: number): Promise<string> {
+    // const wallet = wallets[i];
+    console.log("start toPrivkey:", toPrivkeyStart, "perAccountBatchSendNumOnce", perAccountBatchSendNumOnce)
+    let toPrivkey = toPrivkeyStart;
+    const gasPrice = await provider.getFeeData().then(feeData => feeData.gasPrice);
+    // 获取当前 nonce
+    var nonce = await wallet.getNonce();
+    for (let j = 0; j < perAccountBatchSendNumOnce; j++) {
+
+        const to = config.IS_RECEIVER_BY_PK ?
+            new ethers.Wallet(numberToPrivateKey(toPrivkey), provider).address :
+            wallet.address;
+
+        try {
+            // 创建交易数据
+            const tx: ethers.TransactionRequest = {
+                nonce,
+                gasLimit: 21000, // 适用于简单的转账交易
+                gasPrice: gasPrice!,
+                to,
+                value: ethers.parseEther("0.000000000000000001"), // 每次转账 1 wei
+                data: "0x", // 数据字段为空
+            };
+
+            // 签名并发送交易
+            const txResp = await wallet.sendTransaction(tx);
+            console.log(`sent tx, process ${processId} wallet ${walletIndex} nonce ${nonce} to ${to} pk ${toPrivkey} hash ${txResp.hash}`);
+            nonce++;
+            toPrivkey++;
+
+            if (j == perAccountBatchSendNumOnce - 1) {
+                return txResp.hash;
+            }
+        } catch (error) {
+            console.error(`Failed to send transaction for ${wallet.address}:`, error);
+        }
+    }
+    throw new Error("send tx failed")
 }
 
 function parseArgv() {
@@ -67,7 +76,7 @@ function parseArgv() {
     // 进程 id，用于计算from的地址和 to 的地址； 
     // from 地址为从 id*WALLET_COUNT+1 ～ id*WALLET_COUNT+WALLET_COUNT; 
     // to 地址为从 id*txNum+1 ～ id*txNum+txNum
-    const processId = parseInt(process.argv[2]); 
+    const processId = parseInt(process.argv[2]);
     const totalTxNum = parseInt(process.argv[3]);
     return { processId, totalTxNum };
 }
--- tps.ts
+++ tps.ts
@@ -5,8 +5,9 @@ async function monitorTPS() {
     console.log("monitorTPS start")
     const provider = new ethers.JsonRpcProvider(config.L2_RPC_URL);
 
-    const latestBlockNum = await provider.getBlockNumber();
-    let blockNum = latestBlockNum - 10;
+    const latestBlockNum = parseInt(process.argv[2]) || await provider.getBlockNumber();
+    const blockCnt = 11;
+    let blockNum = latestBlockNum - blockCnt;
 
     const blockCaches: { time: number, block: number, txNum: number }[] = [];
 
@@ -15,19 +16,20 @@ async function monitorTPS() {
         const block = await provider.getBlock(blockNum);
         if (block) {
             blockCaches.push({ time: block.timestamp, block: blockNum, txNum: block.transactions.length });
-            if (blockCaches.length > 10) {
+            if (blockCaches.length > blockCnt) {
                 blockCaches.shift(); // 移除最旧的时间戳
             }
 
-            if (blockCaches.length === 10) {
+            // 计算从第2个开始区块的 TPS，因为第1个区块的时间戳到第N个区块的时间戳产生了第2-N个区块
+            if (blockCaches.length === blockCnt) {
                 const timeDifference = blockCaches[blockCaches.length - 1].time - blockCaches[0].time;
-                const allTxNum = blockCaches.reduce((sum, item) => sum + item.txNum, 0);
+                const allTxNum = blockCaches.slice(1).reduce((sum, item) => sum + item.txNum, 0);
                 const tps = allTxNum / timeDifference;
-                console.log(`Block ${blockCaches[0].block} ~ ${blockNum}, TxNum: ${allTxNum}, TPS: ${tps}`);
+                console.log(`Block ${blockCaches[1].block} ~ ${blockNum}, TxNum: ${allTxNum}, TPS: ${tps}`);
             }
             blockNum++;
         }        
-        await sleep(1000);
+        await sleep(50);
     }
 }
 
